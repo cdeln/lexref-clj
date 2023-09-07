55858deaf79d497a64698e95c2d676b8f951b12c
Carl Dehlin <carl.dehlin@rooniq.com>
2023-09-07 09:44:50 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lexref "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lexref "0.1.0"
+  :description "Lexical references in Clojure"
+  :url "https://github.com/cdeln/lexref-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
